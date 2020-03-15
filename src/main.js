303216a7c1288d19b0214a7099974529084a9727
123456 <123456@example.com>
2020-03-15 21:02:29 +0800

    完成layout组件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import Vant from 'vant' // 引入vant组件
 import 'vant/lib/index.css' // 引入vant组件样式
 import 'amfe-flexible' // 引入插件实时改变font-size大小
+import '@/styles/index.less' // 引入自定义的全局样式  覆盖vant的样式
 
 Vue.config.productionTip = false
 Vue.use(Vant) // 全局注册使用组件
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    公用布局模块
+  <div class="container">
+    <!-- 头部导航栏 -->
+    <!-- fixed将导航固定在顶部 -->
+    <van-nav-bar title="黑马头条" right-text="搜索" @click-right="$router.push('/search')" fixed v-if="$route.path!=='/user'"></van-nav-bar>
+    <!-- 二级路由模块 -->
+    <div class='my-wrapper' :class="{noTop: $route.path === '/user'}">
+      <router-view></router-view>
+    </div>
+    <!-- 底部路由栏 -->
+    <van-tabbar route>
+      <van-tabbar-item icon='home-o' to='/'>首页</van-tabbar-item>
+      <van-tabbar-item icon='chat-o' to='/question'>问答</van-tabbar-item>
+      <van-tabbar-item icon='tv-o' to='/video'>视频</van-tabbar-item>
+      <van-tabbar-item icon='user-o' to='/user'>我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
@@ -10,6 +23,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less'>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 
 </style>
