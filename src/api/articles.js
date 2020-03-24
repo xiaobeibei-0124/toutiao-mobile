e55e93a94870a5383cf061960dfc08b44bfa9cae
123456 <123456@example.com>
2020-03-24 22:43:51 +0800

    文章详情-评论组件-评论列表渲染

--- src/api/articles.js
+++ src/api/articles.js
@@ -61,3 +61,12 @@ export function getArticleInfo (artId) {
     url: `/articles/${artId}`
   })
 }
+/**
+ * 获取文章评论
+ */
+export function getComments (params) {
+  return request({
+    url: '/comments',
+    params
+  })
+}
--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="comment">
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了">
-      <div class="item van-hairline--bottom van-hairline--top" v-for="index in 5" :key="index">
+    <van-list v-model="loading" :finished="finished" @load="onLoad" finished-text="没有更多了">
+      <div class="item van-hairline--bottom van-hairline--top" v-for="comment in comments" :key="comment.com_id.toString()">
         <van-image
           round
           width="1rem"
           height="1rem"
           fit="fill"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          :src="comment.aut_photo"
         />
         <div class="info">
           <p>
-            <span class="name">一阵清风</span>
+            <span class="name">{{comment.aut_name}}</span>
             <span style="float:right">
               <span class="van-icon van-icon-good-job-o zan"></span>
-              <span class="count">10</span>
+              <span class="count">{{comment.like_count}}</span>
             </span>
           </p>
-          <p>评论的内容，。。。。</p>
+          <p>{{comment.content}}</p>
           <p>
-            <span class="time">两天内</span>&nbsp;
-            <van-tag plain @click="showReply=true">4 回复</van-tag>
+            <span class="time">{{comment.pubdate | relTime}}</span>&nbsp;
+            <van-tag plain @click="showReply=true">{{comment.reply_count}} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -37,6 +37,7 @@
 </template>
 
 <script>
+import { getComments } from '@/api/articles'
 export default {
   data () {
     return {
@@ -47,7 +48,29 @@ export default {
       // 输入的内容
       value: '',
       // 控制提交中状态数据
-      submiting: false
+      submiting: false,
+      comments: [], // 用来存放评论列表的数据
+      offset: null // 表示分页依据 如果为空,表示从第一页开始
+    }
+  },
+  methods: {
+    async onLoad () {
+      const { artId } = this.$route.query
+      const data = await getComments({
+        type: 'a',
+        source: artId,
+        offset: this.offset
+      })
+      // 获取回来插入到评论后边
+      this.comments.push(...data.results)
+      this.loading = false
+      // 判断是否还有下一页
+      this.finished = data.end_id === data.last_id
+      if (!this.finished) {
+        // 表示 还没没结束
+        // data.last_id是 当前页的最后一个id
+        this.offset = data.last_id
+      }
     }
   }
 }
