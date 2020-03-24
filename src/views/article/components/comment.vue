34f3c8b437d8e5c3611c66660c6887a84185c134
123456 <123456@example.com>
2020-03-24 22:55:34 +0800

    文章详情-评论组件-回复列表布局

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -20,7 +20,7 @@
           <p>{{comment.content}}</p>
           <p>
             <span class="time">{{comment.pubdate | relTime}}</span>&nbsp;
-            <van-tag plain @click="showReply=true">{{comment.reply_count}} 回复</van-tag>
+            <van-tag plain @click="openReply">{{comment.reply_count}} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -31,6 +31,19 @@
         <span class="submit" v-else slot="button">提交</span>
       </van-field>
     </div>
+     <!-- 评论的回复评论列表 -->
+    <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
+      <van-list v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
+        <div class="item van-hairline--bottom van-hairline--top" v-for="index in 8" :key="index">
+          <van-image round width="1rem" height="1rem" fit="fill" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <div class="info">
+            <p><span class="name">一阵清风</span></p>
+            <p>评论的内容，。。。。</p>
+            <p><span class="time">两天内</span></p>
+          </div>
+        </div>
+      </van-list>
+    </van-action-sheet>
   </div>
 
   <!-- 都不输入框 -->
@@ -50,10 +63,23 @@ export default {
       // 控制提交中状态数据
       submiting: false,
       comments: [], // 用来存放评论列表的数据
-      offset: null // 表示分页依据 如果为空,表示从第一页开始
+      offset: null, // 表示分页依据 如果为空,表示从第一页开始
+      showReply: false, // 控制评论的评论面板是否显示
+      reply: {
+        // 此对象专门放置 面板加载信息
+        loading: false, // 评论的评论的加载状态
+        finished: false, // 评论的评论是否加载完毕
+        offset: null, // 偏移量 作为评论的评论分页加载的时候 查询的依据
+        list: [] // 存放 评论的评论的数据
+      }
     }
   },
   methods: {
+    // 打开评论的回复评论列表
+    openReply () {
+      this.showReply = true // 打开
+      // 处理
+    },
     async onLoad () {
       const { artId } = this.$route.query
       const data = await getComments({
@@ -77,6 +103,25 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.reply_dialog {
+  height: 100%;
+  max-height: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  .van-action-sheet__header {
+    background: #3296fa;
+    color: #fff;
+    .van-icon-close {
+      color: #fff;
+    }
+  }
+  .van-action-sheet__content{
+    flex: 1;
+    overflow-y: auto;
+    padding: 0 10px 44px;
+  }
+}
 .comment {
   margin-top: 10px;
   /deep/ .item {
