f752258677c0b2668ae06fb0b3b4572ff99ebc44
123456 <123456@example.com>
2020-03-25 00:16:59 +0800

    文章详情-评论组件-回复列表渲染

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -20,7 +20,7 @@
           <p>{{comment.content}}</p>
           <p>
             <span class="time">{{comment.pubdate | relTime}}</span>&nbsp;
-            <van-tag plain @click="openReply">{{comment.reply_count}} 回复</van-tag>
+            <van-tag plain @click="openReply(comment.com_id.toString())">{{comment.reply_count}} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -33,13 +33,14 @@
     </div>
      <!-- 评论的回复评论列表 -->
     <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
-      <van-list v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
-        <div class="item van-hairline--bottom van-hairline--top" v-for="index in 8" :key="index">
-          <van-image round width="1rem" height="1rem" fit="fill" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      <!-- 关闭第一次默认加载 -->
+      <van-list @load="getReply" :immediate-check="false" v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
+        <div class="item van-hairline--bottom van-hairline--top" v-for="item in reply.list" :key="item.com_id.toString()">
+          <van-image round width="1rem" height="1rem" fit="fill" :src="item.aut_photo" />
           <div class="info">
-            <p><span class="name">一阵清风</span></p>
-            <p>评论的内容，。。。。</p>
-            <p><span class="time">两天内</span></p>
+            <p><span class="name">{{item.aut_name}}</span></p>
+            <p>{{item.content}}</p>
+            <p><span class="time">{{ item.pubdate | relTime }}</span></p>
           </div>
         </div>
       </van-list>
@@ -65,21 +66,46 @@ export default {
       comments: [], // 用来存放评论列表的数据
       offset: null, // 表示分页依据 如果为空,表示从第一页开始
       showReply: false, // 控制评论的评论面板是否显示
+      // 此对象专门放置 面板加载信息
       reply: {
-        // 此对象专门放置 面板加载信息
         loading: false, // 评论的评论的加载状态
         finished: false, // 评论的评论是否加载完毕
         offset: null, // 偏移量 作为评论的评论分页加载的时候 查询的依据
-        list: [] // 存放 评论的评论的数据
+        list: [], // 存放 评论的评论的数据
+        commentId: null
       }
     }
   },
   methods: {
     // 打开评论的回复评论列表
-    openReply () {
+    openReply (commentId) {
       this.showReply = true // 打开
       // 处理
+      this.reply.commentId = commentId
+      // 在弹出面板的时候 重置数据
+      //  弹出窗口之前  把之前的list清空  重置数据
+      this.reply.list = []
+      this.reply.offset = null // 点击评论的时候是新的数据，所有数据重置
+      this.reply.finished = false
+      this.reply.loading = true
+      // 手动关掉主动请求就要手动打开
+      this.getReply() // 弹出评论的评论的层时 主动的去请求一次数据
     },
+    // 获取回复评论的内容
+    async getReply () {
+      const data = await getComments({
+        type: 'c',
+        source: this.reply.commentId,
+        offset: this.reply.offset
+      })
+      this.reply.list.push(...data.results)
+      this.reply.loading = false
+      this.reply.finished = data.end_id === data.last_id
+      if (!this.reply.finished) {
+        this.reply.offset = data.last_id
+      }
+    },
+    // 获取评论内容
     async onLoad () {
       const { artId } = this.$route.query
       const data = await getComments({
